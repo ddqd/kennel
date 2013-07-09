0e52632ae009246ccd128cfbcdf39081784e765d
ddqd <diman@ya.ru>
2013-07-09 23:01:55 +0400

    upd from master

--- dog.js
+++ dog.js
@@ -10,7 +10,7 @@ Image.prototype.setY = function setY (y) {
 }
 
 function clear () {
-	ctx.clearRect ( 0 , 0 , 800 , 600 );
+	ctx.clearRect (0, 0, canvas.width, canvas.height);
 }
 
 function rnd(min, max) {
@@ -19,16 +19,15 @@ function rnd(min, max) {
 
 function generateDog() {
 	var dog = new Image;
-	dog.width = rnd(50,70);
-	dog.height = rnd(50,70);
-	dog.X = rnd(30,500);
-	dog.Y = rnd(30,500);
+	dog.width = rnd(80,100);
+	dog.height = rnd(80,100);
+	dog.setX(rnd(30,500));
+	dog.setY(rnd(30,500));
 	dog.src = 'dog.png';
 	return dog;
 }
 
 var dogs = new Array();
-
 
 function createDog() {
 	for (var i=0; i<10; i++) {
@@ -47,7 +46,6 @@ function startMove() {
 }
 
 function move(dog) {
-
 	dog.X = rnd(dog.X-5, dog.X+5);
 	dog.Y = rnd(dog.Y-5, dog.Y+5);
 	ctx.drawImage(dog, dog.X , dog.Y, dog.width, dog.height);
@@ -62,21 +60,34 @@ function moveTimer() {
 	 var timer = setInterval((function(){startMove()}),rnd(200,1500))
 }
 
+function getMousePos(canvas, e) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: e.clientX - rect.left,
+          y: e.clientY - rect.top
+        };
+      }
+
 canvas.addEventListener('click', function(e) {
-   var pos = {
-       x : e.pageX-400,
-       y : e.pageY-100
-   };
-   addDog(pos);
+    addDog(e);
 }, false);
 
+canvas.addEventListener('mousemove', function(e) {
+    addDog(e);
+}, false);
 
-function addDog(pos) {
-	var dog = generateDog();
-	dog.X = pos.x;
-	dog.Y = pos.y;
+function addDog(e) {
+    var dog = generateDog();
+    var pos = getMousePos(canvas, e);
+    dog.X = pos.x - dog.width/2
+    dog.Y = pos.y - dog.height/2
+    drawDog(dog);
+}
+
+function drawDog(dog) {
+    clear();
 	dogs[dogs.length] = dog;
-
-for(var i = 0; i<dogs.length; i++)
-	ctx.drawImage(dogs[i], dogs[i].X ,dogs[i].Y, dogs[i].width, dogs[i].height);
+    for(var i = 0; i<dogs.length; i++) {
+        ctx.drawImage(dogs[i], dogs[i].X ,dogs[i].Y, dogs[i].width, dogs[i].height);
+    }
 }
